354e2d9911dbb2f051d031e13f6e67358ea6ef5d
RohinPalsule <122756013+RohinPalsule@users.noreply.github.com>
2024-04-04 23:53:09 -0700

    Remaining survey JS files

---
+++ AUDIT.js
@@ -0,0 +1,42 @@
+var AUDITQuestionsMatrix = [
+    {
+        question: "How often do you have a drink containing alcohol?",
+        choices: ["Never", "Monthly or less", "2 to 4 times a month", "2 to 3 times a week", "4 or more times a week"]
+    },
+    {
+        question: "How many drinks containing alcohol do you have on a typical day when you are drinking?",
+        choices: ["1 or 2", "3 or 4", "5 or 6", "7,8, and 9", "10 or more"]
+    },
+    {
+        question: "How often do you have six or more drinks on one occasion?",
+        choices: ["Never", "Less than monthly", "Monthly", "Weekly", "Daily or almost daily"]
+    },
+    {
+        question: "How often during the last year have you found that you were not able to stop drinking once you had started?",
+        choices: ["Never", "Less than monthly", "Monthly", "Weekly", "Daily or almost daily"]
+    },
+    {
+        question: "How often during the last year have you failed to do what was normally expected of you because of drinking?",
+        choices: ["Never", "Less than monthly", "Monthly", "Weekly", "Daily or almost daily"]
+    },
+    {
+        question: "How often during the last year have you needed a first drink in the morning to get yourself going after a heavy drinking session?",
+        choices: ["Never", "Less than monthly", "Monthly", "Weekly", "Daily or almost daily"]
+    },
+    {
+        question: "How often during the last year have you had a feeling of guilt or remorse after drinking?",
+        choices: ["Never", "Less than monthly", "Monthly", "Weekly", "Daily or almost daily"]
+    },
+    {
+        question: "How often during the last year have you been unable to remember what happened the night before because of your drinking?",
+        choices: ["Never", "Less than monthly", "Monthly", "Weekly", "Daily or almost daily"]
+    },
+    {
+        question: "Have you or someone else been injured because of your drinking?",
+        choices: ["No", "Yes, but not in the last year", "Yes, during the last year"]
+    },
+    {
+        question: "Has a relative, friend, doctor, or other health care worker been concerned about your drinking or suggested you cut down?",
+        choices: ["No", "Yes, but not in the last year", "Yes, during the last year"]
+    },
+];
